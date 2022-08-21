51a563f759343403e4b8e55363079819863e5c0d
Remix Workspace <>
2022-08-21 20:52:58 +0500

    August 21 Assignment Tasks

--- contracts/5_aug 21-Task-1.sol
+++ contracts/5_aug 21-Task-1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.15;
 
-// August 21 - Assignment (Task-1) | Muhammad Taha.
+// August 21 - Assignment (Task-1) | Muhammad Taha
 contract printTask
 
 {
--- contracts/6_aug 21-Task-2.sol
+++ contracts/6_aug 21-Task-2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.15;
 
-// August 21 - Assignment (Task-2) | Muhammad Taha.
+// August 21 - Assignment (Task-2) | Muhammad Taha
 contract task2
 {
     uint myVariable = 10;
